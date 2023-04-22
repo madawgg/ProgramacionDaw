335626cebf4feaf0ad453ab1680ea1895568f158
madawgg <migueldaw24@gmail.com>
2023-04-22 18:54:02 +0200

    simulacion simple de un pequeño ecosistema

---
+++ la.vida.dels.microorganismes/src/laVidaDelsMicroorganismes/Aliment.java
@@ -0,0 +1,10 @@
+package laVidaDelsMicroorganismes;
+
+//aquesta es una clase enumerada que nomes serveix per a enumerar els tipus d'aliments que menja cada esser
+public enum Aliment { 
+    AMEBA,
+    BACTERI,
+    ALGA,
+    NUTRIENTS,
+    TOT;
+}
